e9d625a44b73d6ee1e413ecb0188665e3d03faf8
Christopher Biscardi <biscarch@sketcht.com>
2019-01-03 19:39:58 -0800

    Added Code component
    * Removed gatsby-remark-prismjs
    * Code component supports prism highlighting and react-live arbitrary
      code execution

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,3 @@
-// custom typefaces
-import 'typeface-montserrat'
-import 'typeface-merriweather'
-
 import { wrapRootElement as wrap } from './wrap-root-element'
 
 export const wrapRootElement = wrap
